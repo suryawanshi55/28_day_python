ae332fe1d4fb5668c5c21ce291b30c43009196e5
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-08 14:29:36 +0530

    Add files via upload

---
+++ operater.py
@@ -0,0 +1,57 @@
+# .....# Assignment Operator.....
+# a=5
+# a+=2
+# print (a)7
+# print ("the value of p after p+=2 is {p}".format(p=3))the value of p after p+=2 is 3
+# b=12
+# b/=2
+# print(b)  6.0
+# Arithmetic operators
+# a=float (input("enter the value of a:"))  enter the value of a:65.0
+# b=float(input("enter the value of b:"))enter the value of b:5.5
+# a,b=[int(x) for x in input('enter the value of a and b:')]
+# print(f"the value of {a}+{b} is{a+b}")
+# print(f"the value of {a}-{b} is{a-b}")
+# print(f"the value of {a}*{b} is{a*b}")
+# print(f"the value of {a}/{b} is{a/b}")
+# print(f"the value of {a}%{b} is{a%b}")
+# print(f"the value of {a}**{b} is{a**b}")
+#.... floar division....
+# c=4//2
+# print(c)   #2
+# d=4//2.0
+# print(d)  2.0
+# e=5//2
+# print(e)    2
+# f=5.0//2
+# print(f)  2.0
+# g=5.0/2
+# print(g)   2.5
+# .....assignment  operator.....
+# a=2;b=3
+# a=a+b
+# print(a) 5   
+# a=a+a
+# print(a)  10
+# a=5
+# a+=3
+# print(a)   8
+# a=-2
+# print(a)    -2
+# a=3
+# a*=4 
+# print(a)     12
+# a=5
+# a/2
+# print(a)   2.5
+# v=5
+# v**=3
+# print(v)   125
+# p=int(input('enter the value of p:'))     enter the value of p:25
+# print(f"the value of p is{p}")            the value of p is25
+
+
+
+
+
+
